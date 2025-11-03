9cb593f6db38705e6c219f94f3c1858a7b65fc83
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-03 13:35:23 +0900

    learn: useState Functional Update Form

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    setValue(currentState => {
+      const newState = currentState + 1;
+      return newState;
+    });
+  };
+
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        increase
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
