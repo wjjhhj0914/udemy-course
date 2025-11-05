47b4baa8d6f8bdb465130c25293082101ab206ca
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-05 21:19:25 +0900

    learn: useEffect - Multiple Effects

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from './tutorial/01-useState/starter/05-useState-gotcha';
-import Final from './tutorial/01-useState/final/05-useState-gotcha';
+import Starter from './tutorial/02-useEffect/starter/03-multiple-effects';
+import Final from './tutorial/02-useEffect/final/03-multiple-effects';
 
 function App() {
   return (
