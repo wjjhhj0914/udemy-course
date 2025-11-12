745d3fa3f8120d785f6c8b9644822e827a6737f4
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-12 22:30:05 +0900

    learn: useEffect Fetch Challenge

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from './tutorial/02-useEffect/starter/03-multiple-effects';
-import Final from './tutorial/02-useEffect/final/03-multiple-effects';
+import Starter from './tutorial/02-useEffect/starter/04-fetch-data';
+import Final from './tutorial/02-useEffect/final/04-fetch-data';
 
 function App() {
   return (
