a234465f920c64d534691f0b9f411545a4f13961
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-13 23:04:59 +0900

    learn: Multiple Returns

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from './tutorial/02-useEffect/starter/04-fetch-data';
-import Final from './tutorial/02-useEffect/final/04-fetch-data';
+import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics';
+import Final from './tutorial/03-conditional-rendering/final/01-multiple-returns-basics';
 
 function App() {
   return (
