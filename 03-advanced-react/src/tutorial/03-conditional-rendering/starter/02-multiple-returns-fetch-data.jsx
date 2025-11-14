8b39860f30cd0e472d6f36f3ffd956869429ba39
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-14 21:59:52 +0900

    learn: Fetch Error "Gotcha"

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -4,12 +4,20 @@ const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturnsFetchData = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const response = await fetch(url);
+
+        if (!response.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
+
         const user = await response.json();
         setUser(user);
       } catch (error) {
