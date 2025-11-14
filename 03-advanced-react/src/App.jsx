4f476de4e8e2b1f255cb0c3c8846e916e1901d6d
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-14 22:15:29 +0900

    learn: Order Matters

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="container">
       <Starter />
-      {/* <Final /> */}
+      <Final />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -34,16 +34,18 @@ const MultipleReturnsFetchData = () => {
     return <h2>There was an error...</h2>;
   }
 
+  const { avatar_url, name, company, bio } = user;
+
   return (
     <div>
       <img
         style={{ width: '150px', borderRadius: '25px' }}
-        src={user.avatar_url}
-        alt={user.name}
+        src={avatar_url}
+        alt={name}
       />
-      <h2>{user.name}</h2>
-      <h4>works at {user.company}</h4>
-      <p>{user.bio}</p>
+      <h2>{name}</h2>
+      <h4>works at {company}</h4>
+      <p>{bio}</p>
     </div>
   );
 };
