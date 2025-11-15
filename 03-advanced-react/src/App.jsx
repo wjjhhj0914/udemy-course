234dc5a21c1e0e9c3d03aa04eed461a9608b3b98
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-15 22:09:19 +0900

    learn: Short Circuit

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,11 @@
-import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data';
-import Final from './tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data';
+import Starter from './tutorial/03-conditional-rendering/starter/04-short-circuit-overview';
+import Final from './tutorial/03-conditional-rendering/final/04-short-circuit-overview';
 
 function App() {
   return (
     <div className="container">
       <Starter />
-      <Final />
+      {/* <Final /> */}
     </div>
   );
 }
