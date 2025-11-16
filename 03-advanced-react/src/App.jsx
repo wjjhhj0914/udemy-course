ec983d209fb8ef09ff8bf6761578b98d49873098
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-11-16 21:20:12 +0900

    learn: Toggle Challenge

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from './tutorial/03-conditional-rendering/starter/05-short-circuit-examples';
-import Final from './tutorial/03-conditional-rendering/final/05-short-circuit-examples';
+import Starter from './tutorial/03-conditional-rendering/starter/06-toggle-challenge';
+import Final from './tutorial/03-conditional-rendering/final/06-toggle-challenge';
 
 function App() {
   return (
